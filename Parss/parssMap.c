0866eea7fdd92adc351104e599754f2db92079fb
Issam Elmakhfi <ielmakhf@e1r9p3.1337.ma>
2023-01-26 20:18:38 +0100

    still in parss :(

--- Parss/parssMap.c
+++ Parss/parssMap.c
@@ -6,7 +6,7 @@
 /*   By: ielmakhf <ielmakhf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 16:40:07 by ielmakhf          #+#    #+#             */
-/*   Updated: 2023/01/25 20:17:25 by ielmakhf         ###   ########.fr       */
+/*   Updated: 2023/01/26 20:17:09 by ielmakhf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,8 +39,7 @@ char    *get_filename_ext(char *filename)
 
 int get_direction(char **direction)
 {
-    // int fd;
-
+    printf("HERE1\n");
     if (direction[0] && !ft_strcmp(direction[0], "NO"))
         check_path(direction[1]);
     else if (direction[0] && !ft_strcmp(direction[0], "SO"))
@@ -50,23 +49,57 @@ int get_direction(char **direction)
     else if (direction[0] && !ft_strcmp(direction[0], "EA"))
         check_path(direction[1]);
     else
-        return (2);    
+        return (1);
     return (0);
 }
 
-int check_directions(t_map *map)
+int check_digit(char *str)
+{
+    int i = 0;
+
+    while (str[i])
+    {
+        if (!ft_isdigit(str[i++]))
+            return (1);
+    }
+    return (0);
+}
+
+int check_colors(char **colors)
+{
+    int i = 1;
+    char    *tmp;
+
+    if (colors[0] && (!ft_strcmp(colors[0], "F") || !ft_strcmp(colors[0], "C")))
+    {
+        while (colors[i])
+        {
+            tmp = ft_strtrim(colors[i], ",");
+            if (check_digit(tmp))
+                return (1);
+            if (ft_atoi(tmp) > 256)
+                return (1);
+            i++;
+        }
+    }
+    return (0);
+}
+
+int check_elements(t_map *map)
 {
     int i;
-    char **dire;
+    char **elements;
 
     i = 0;
     while (map->map_tab[i])
     {
-        dire = ft_split(map->map_tab[i], ' ');
-        if (get_direction(dire) == 1)
+        elements = ft_split(map->map_tab[i], ' ');
+        if (get_direction(elements))
             return (1);
-        free_tab(dire);
-        dire = NULL;
+        if (check_colors(elements))
+            return (1);
+        free_tab(elements);
+        elements = NULL;
         i++;
     }
     return (0);
@@ -89,7 +122,9 @@ int parss_map(t_map *map, char *av)
         i++;
     }
     map->map_tab[i] = NULL;
-    check_directions(map);
+    if (check_elements(map))
+        error_handler("ELEMENTS ERROR", 1);
+    // check_colors(map);
     // free_tab(map->map_tab);
     return (0);
 }
