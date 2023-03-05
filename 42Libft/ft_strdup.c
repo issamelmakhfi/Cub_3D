e76b88fdc8e51b2d2326cb2c7b9012623b3f052b
Issam Elmakhfi <ielmakhf@e1r9p5.1337.ma>
2023-03-05 18:53:50 +0100

    working on some changes

---
+++ 42Libft/ft_strdup.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ielmakhf <ielmakhf@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/02 18:37:01 by ielmakhf          #+#    #+#             */
+/*   Updated: 2023/02/05 13:43:09 by ielmakhf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *src)
+{
+	int		i;
+	char	*s;
+
+	i = 0;
+	if (!src)
+		return (NULL);
+	s = malloc(ft_strlen((char *)src) + 1);
+	if (!s)
+		return (NULL);
+	while (src[i])
+	{
+		s[i] = src[i];
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
---
+++ 42Libft/ft_strncmp.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ielmakhf <ielmakhf@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/08 19:05:09 by ielmakhf          #+#    #+#             */
+/*   Updated: 2023/03/02 17:46:21 by ielmakhf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while ((s1[i] || s2[i]) && s1[i] == s2[i] && n != 0)
+	{
+		i++;
+		n--;
+	}
+	if (n == 0)
+		return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
